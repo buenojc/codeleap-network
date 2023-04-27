d334cf56ad28006a5ae9285f3b9b2b704387dfbc
Julio Cesar Bueno <buenojc@outlook.com>
2023-04-27 13:21:30 -0300

    feat: add actions buttons

--- src/components/PostCard/index.jsx
+++ src/components/PostCard/index.jsx
@@ -1,10 +1,21 @@
 import styles from './postCard.module.css'
+import DeleteIcon from '../../assets/delete-icon.svg'
+import EditIcon from '../../assets/edit-icon.svg'
 
 export default function PostCard(){
     return(
         <div className={styles.postCardWrapper}>
             <div className={styles.header}>
                 <h2>My First Post</h2>
+
+                <div className={styles.actionsWrapper}>
+                    <span>
+                        <img src={DeleteIcon} alt='Delete Post' />
+                    </span>
+                    <span>
+                        <img src={EditIcon} alt='Edit Post' />
+                    </span>
+                </div>
             </div>
             <div className={styles.content}>
                 <div className={styles.informationWrapper}>
