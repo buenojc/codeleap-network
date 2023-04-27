2022412c97377bd35da06d273cde7eedbba77f5c
Julio Cesar Bueno <buenojc@outlook.com>
2023-04-26 23:57:01 -0300

    feat: create Textarea component

---
+++ src/components/Textarea/index.jsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import styles from "./textarea.module.css";
+
+export default function Textarea({
+  children,
+  name,
+  placeholder,
+  value,
+  onChange,
+}) {
+  return (
+    <div className={styles.inputWrapper}>
+      <label className="labelInput" htmlFor={name}>
+        {children}
+      </label>
+      <textarea
+        // value={value}
+        name={name}
+        // id={name}
+        placeholder={placeholder}
+        // onChange={onChange}
+      ></textarea>
+    </div>
+  );
+}
