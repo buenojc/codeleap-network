b5a73c39e44fb900d0de0737c13f27b799f638ae
Julio Cesar Bueno <buenojc@outlook.com>
2023-04-30 19:48:39 -0300

    chore: remove deprecated createstore

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
-import reducers from './reducers'
+import { configureStore } from "@reduxjs/toolkit";
+import { postReducer } from "./postReducer";
+import { loginReducer } from "./loginReducer";
+import { errorReducer } from "./errorReducer";
 
-const store = createStore(reducers, {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+const store = configureStore({
+    reducer: {
+        posts: postReducer,
+        user: loginReducer,
+        error: errorReducer
+    }
+  })
 
 export default store;
